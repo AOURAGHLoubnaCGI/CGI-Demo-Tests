09a2c6348dea7cbe2b8a861d156d31d7582aac2c
AOURAGH <loubna.aouragh@cgi.com>
2024-05-13 16:33:17 +0100

    Key integration

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   // Configure the reporter for generating reports
   reporter: 'cypress-mochawesome-reporter',
-  
+  projectId: "f2n1bu",
   e2e: {
     setupNodeEvents(on, config) {
       // Setup for the cypress-mochawesome-reporter
