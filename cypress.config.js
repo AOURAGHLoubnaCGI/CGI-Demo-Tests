a5f02f59259fbf669432e594b239977863fb28b6
AOURAGH <loubna.aouragh@cgi.com>
2024-05-14 13:48:37 +0100

    yml modification

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'eo1v5q',
   // Configure the reporter for generating reports
   reporter: 'cypress-mochawesome-reporter',
-  projectId: "f2n1bu",
+  //projectId: "f2n1bu",
   e2e: {
     setupNodeEvents(on, config) {
       // Setup for the cypress-mochawesome-reporter
